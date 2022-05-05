a197fe3c44126543dc4e416b6d821396d554a70b
Chris Judkins <chrisjudk@protonmail.com>
2022-05-04 22:32:13 -0400

    Create WhitelistMaker.py

---
+++ WhitelistMaker.py
@@ -0,0 +1,28 @@
+try:
+    import os
+    from simple_term_menu import TerminalMenu
+except ImportError as e:
+    print("There was an error while importing required modules!",
+    end="\n================================================================\n")
+    print(f"{e.name} is a required package!")
+##################################################################################
+# Main menu builder
+path = os.getcwd()
+catPath = path + "/whitelist-categories/"
+categories = os.listdir(catPath)
+
+options = []
+for cat in categories:
+    options.append(cat)
+options.append("Quit")
+
+menu = TerminalMenu(options)
+quit = False
+while quit == false:
+    index = menu.show()
+    option = options[index]
+
+    if(option == "Quit"):
+        quit = True
+    else:
+        print(option)
