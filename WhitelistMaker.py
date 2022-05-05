d13554b84ab2ca0f6945033f055e0b7ea9e5235f
chrisjudk <chrisjudk@protonmail.com>
2022-05-05 02:14:50 -0400

    directory depth of two

--- WhitelistMaker.py
+++ WhitelistMaker.py
@@ -10,6 +10,16 @@ except ImportError as e:
     sys.exit(os.EX_UNAVAILABLE)
 
 ##################################################################################
+# Functions
+def buildwl():
+    with open(path + "/customWhitelist", "w") as out:
+        for file in selections:
+            with open(catPath + file,"r") as f:
+                lines = f.readlines()
+                for line in lines:
+                    out.write(line)
+        print(f"File saved to: {os.path.realpath(out.name)}")
+##################################################################################
 # Main menu builder
 path = os.getcwd()
 catPath = path + "/whitelist-categories/"
@@ -17,24 +27,22 @@ categories = os.listdir(catPath)
 temp = []
 options = []
 for cat in categories:
-    if os.path.isfile(catPath+cat):
-        options.append(cat)
-    else:
-        temp.clear()
-        for file in os.listdir(catPath + cat + "\"):
-            temp.append(file)
-        options.append(temp)
+    options.append(cat)
 options.append("[a] All")
-options.append("Quit")
-
+options.append("[q] Quit")
+options.append("[b] Build")
 quit = False
 selections = []
 while quit == False:
+    exit = False
     index = TerminalMenu(options).show()
     option = options[index]
 
     if(option == "[q] Quit"):
         quit = True
+    elif(option == "[b] Build"):
+        quit = True
+        buildwl()
     elif(option =="[a] All"):
         for o in options:
             if(o != "[a] All" and o != "[q] Quit"):
@@ -42,16 +50,18 @@ while quit == False:
                 options.remove(o)
         print(selections)
         quit = True
+    elif(os.path.isdir(catPath + option)):
+        temp.clear()
+        temp = os.listdir(catPath + option + "/")
+        temp.append("[b] Back")
+        while exit == False:
+            file = temp[TerminalMenu(temp).show()]
+            if file == "[b] Back":
+                exit = True
+            else:
+                temp.remove(file)
+                selections.append(option + "/" + file)
     else:
         selections.append(option)
         options.remove(option)
         print(selections)
-
-##################################################################################
-# Build the list
-with open(path + "/customWhitelist", "w") as out:
-    for file in selections:
-        with open(catPath + file,"r") as f:
-            lines = f.readlines()
-            for line in lines:
-                out.write(line)
