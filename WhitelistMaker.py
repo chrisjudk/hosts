775d76c3ce084823a0ed7d4cccb75d749ca77a64
chrisjudk <chrisjudk@protonmail.com>
2022-05-05 01:39:13 -0400

    Updated WhitelistMaker.py

--- WhitelistMaker.py
+++ WhitelistMaker.py
@@ -1,12 +1,14 @@
 try:
     import os
     import sys
+    from pprint import pprint
     from simple_term_menu import TerminalMenu
 except ImportError as e:
     print("There was an error while importing required modules!",
     end="\n================================================================\n")
     print(f"{e.name} is a required package!")
     sys.exit(os.EX_UNAVAILABLE)
+
 ##################################################################################
 # Main menu builder
 path = os.getcwd()
@@ -16,15 +18,34 @@ categories = os.listdir(catPath)
 options = []
 for cat in categories:
     options.append(cat)
-options.append("Quit")
+options.append("[a] All")
+options.append("[q] Quit")
 
-menu = TerminalMenu(options)
 quit = False
+selections = []
 while quit == False:
-    index = menu.show()
+    index = TerminalMenu(options).show()
     option = options[index]
-
-    if(option == "Quit"):
+    
+    if(option == "[q] Quit"):
+        quit = True
+    elif(option =="[a] All"):
+        for o in options:
+            if(o != "[a] All" and o != "[q] Quit"):
+                selections.append(o)
+                options.remove(o)
+        print(selections)
         quit = True
     else:
-        print(option)
+        selections.append(option)
+        options.remove(option)
+        print(selections)
+
+##################################################################################
+# Build the list
+with open(path + "/customWhitelist", "w") as out:
+    for file in selections:
+        with open(catPath + file,"r") as f:
+            lines = f.readlines()
+            for line in lines:
+                out.write(line)
